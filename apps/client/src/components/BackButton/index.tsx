74f48fd3cfbd2a13f1e496108c77f419c14d6fbe
efrenruizrubio <efren282@outlook.es>
2023-09-20 07:26:44 -0600

    Refactor: Add svg icon for back arrow

--- apps/client/src/components/BackButton/index.tsx
+++ apps/client/src/components/BackButton/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 import styles from "./BackButton.module.scss";
 
@@ -8,7 +9,7 @@ export default function BackButton(): JSX.Element {
   const { back } = useRouter();
   return (
     <button className={styles.back} onClick={back} type="button">
-      {"<-"}
+      <Image src={"/back_arrow.svg"} alt="Flecha hacia atrás" width={30} height={30}/>
     </button>
   );
 }
